906dc2c86be74fe305ea5a89d2bf1d3808830ab2
Richard Shi <richardshi97@gmail.com>
2017-03-12 13:06:25 -0400

    Changed messages for help and add to be clearer

--- reaction-bot/commands/help.rb
+++ reaction-bot/commands/help.rb
@@ -9,11 +9,12 @@ module ReactionBot
       def self.usage
         "ReactionBot v0.1\n" \
         "COMMANDS:\n" \
-        "add <trigger> <image> => Tell reaction_bot to display <image> when <trigger> is heard\n" \
-        "remove <trigger>      => Stop listening for <trigger>\n" \
-        "get <trigger>         => Ask reaction_bot to show image for <trigger>\n" \
-        "help                  => Show this message\n" \
-        "<trigger>             => NOT DONE: Display corresponding image for <trigger>\n"
+        "add :<trigger>: <image> => Tell reaction_bot to display <image> when <trigger> is heard.\n" \
+        "NOTE: trigger must be surrounded by commas and have no whitespace in it.\n" \
+        "remove <trigger> => Stop listening for <trigger>\n" \
+        "get <trigger> => Ask reaction_bot to show image for <trigger>\n" \
+        "help => Show this message\n" \
+        "<trigger> => Display corresponding image for <trigger>\n"
       end
 
       # TODO: Use web client exclusively for better messages
