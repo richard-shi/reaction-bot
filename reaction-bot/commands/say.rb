27ea358fb286fe820c55885288bc8bc6893dd872
Richard Shi <richardshi97@gmail.com>
2017-03-19 01:00:23 -0400

    Added say command to reactionbot

---
+++ reaction-bot/commands/say.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+module Commands
+  # Tell Reactionbot to say something to a channel
+  class Say < SlackRubyBot::Commands::Base
+    command 'say'
+
+    def self.get_channel_id(channel_name)
+      /<#(?<id>[^>\|]+)(?:\|([^>]+))?>/.match(channel_name)[:id]     
+    end
+
+    def self.call(client, data, match)
+      client.say(channel: data.channel, text: 'Note: Channel name needs a \# at the beginning')
+      channel_name, text = match[:expression].split(" ", 2)
+      channel_id = get_channel_id(channel_name)
+      #channel_id = (client.web_client.channels_info(channel: channel_name)["channel"])["id"]
+      client.say(channel: channel_id, text: text)
+    end
+  end
+end
