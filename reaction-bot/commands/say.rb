ea7a2e91bd5cdd1baf61ae7a9a78ee3f0ba2b2a3
Richard Shi <richardshi97@gmail.com>
2017-03-19 02:26:29 -0400

    Added direct messaging capability to say

--- reaction-bot/commands/say.rb
+++ reaction-bot/commands/say.rb
@@ -1,18 +1,22 @@
 #!/usr/bin/env ruby
 
+#require 'pry-remote'
+
 module Commands
   # Tell Reactionbot to say something to a channel
   class Say < SlackRubyBot::Commands::Base
     command 'say'
 
-    def self.get_channel_id(channel_name)
-      /<[#@](?<id>[^>\|]+)(?:\|([^>]+))?>/.match(channel_name)[:id]     
+    def self.extract_id(escaped_text)
+      /<[#@](?<id>[^>\|]+)(?:\|([^>]+))?>/.match(escaped_text)[:id]
     end
 
     def self.call(client, data, match)
-      channel_name, text = match[:expression].split(" ", 2)
-      channel_id = get_channel_id(channel_name)
-      #channel_id = (client.web_client.channels_info(channel: channel_name)["channel"])["id"]
+      #binding.remote_pry
+      channel_name, text = match[:expression].split(' ', 2)
+      channel_id = extract_id(channel_name)
+
+      channel_id = (client.web_client.im_open(user: channel_id)['channel'])['id'] if channel_id[0] == 'U'
       client.say(channel: channel_id, text: text)
     end
   end
