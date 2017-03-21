d66c0a2dc6d88667ae0284a139cac2f9058fbbc1
Richard Shi <richardshi97@gmail.com>
2017-03-21 19:28:21 -0400

    Removed commented out requires

--- reaction-bot.rb
+++ reaction-bot.rb
@@ -4,13 +4,3 @@ require 'slack-ruby-bot'
 require 'require_all'
 
 require_all 'reaction-bot'
-
-#require 'reaction-bot/commands/add'
-#require 'reaction-bot/commands/remove'
-#require 'reaction-bot/commands/help'
-#require 'reaction-bot/commands/respond'
-#require 'reaction-bot/commands/debug'
-#require 'reaction-bot/commands/get'
-#require 'reaction-bot/commands/list'
-#require 'reaction-bot/data/datastore'
-#require 'reaction-bot/bot'
