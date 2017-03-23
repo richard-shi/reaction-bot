f9f1c43b80ab48f7acfa27884585a37b31013ae7
Richard Shi <richardshi97@gmail.com>
2017-03-23 19:49:42 -0400

    Changed formatting of respond for readability

--- reaction-bot/commands/respond.rb
+++ reaction-bot/commands/respond.rb
@@ -3,18 +3,14 @@
 module ReactionBot
   module Commands
     class Respond < SlackRubyBot::Commands::Base
+      # Encapsulate to allow for json conversion
       def self.encapsulate_in_json(resource)
-        [
-          {
-            'text' => '',
-            'image_url' => resource
-          }
-        ]
+        [{ 'text' => '', 'image_url' => resource }]
       end
 
       scan(/(?<key>:\S+:)/) do |client, data, matches|
         matches.each do |match|
-          url = ReactionBot::Data::Datastore.get(client.team.name, match[0])
+          url = Data::Datastore.get(client.team.name, match[0])
           next if url.nil?
           client.web_client.chat_postMessage(
             channel: data.channel,
