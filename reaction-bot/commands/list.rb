927efcfd835308fe4450742d67dbf36d29d743a0
Richard Shi <richardshi97@gmail.com>
2017-03-24 01:30:16 -0400

    List now displays triggers in sorted format

--- reaction-bot/commands/list.rb
+++ reaction-bot/commands/list.rb
@@ -7,14 +7,14 @@ module ReactionBot
       command 'list'
 
       def self.call(client, data, match)
-        key_list = Data::WordList.words(client.team.name)
+        key_list = Data::WordList.words(client.team.name).sort
 
         # List all trigger word keys
-        trigger_list = "```Trigger word list: \n"
+        trigger_list = "Trigger word list: \n ```"
         key_list.each do |key|
           trigger_list += "#{key}\n"
         end
-        trigger_list += 'To use any of the above, see help command```'
+        trigger_list += '``` To use any of the above, see help command'
 
         # Send message
         client.say(channel: data.channel, text: trigger_list)
